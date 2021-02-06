c5446a211b2d3ca272a76a53fcf22a380a661e07
Trevor S <captinrex191@gmail.com>
2021-02-06 00:56:05 -0600

    Updated Log-In Page.

--- user/auth.php
+++ user/auth.php
@@ -1,5 +1,9 @@
 <?php session_start();
 
+function errorMessage($msg) {
+    $_SESSION['login_errorMessage'] = $msg;
+}
+
 if ($_POST) {
     
     // Database Log In INFO
@@ -14,29 +18,72 @@ if ($_POST) {
 
     // Log In
     if (isset($_POST['login'])) {
+        
+        // Get POST data
+        $username = $_POST['uname'];
+        $password = $_POST['pwd'];
+        
 
         // Create connection
         $conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
+        
         // Check connection
         if ($conn->connect_error) {
-          echo "Connection failed: " . $conn->connect_error;
+            
+            $conn->close();
+            
+            errorMessage("Our database(s) are down. Please try again later.");
+            
+            header("login.php");
+            exit;
+            
         }
 
-        $sql = "SELECT * FROM users";
+        $sql = "SELECT * FROM users where username='$username'";
         $result = $conn->query($sql);
 
+        // If they have an account
         if ($result->num_rows > 0) {
+            
           // output data of each row
           while($row = $result->fetch_assoc()) {
-            echo "userID: " . $row['userID'] . "<br>";
-            echo "username: " . $row['username'] . "<br>";
-            echo "password: " . $row['password'] . "<br>";
-            echo "email: " . $row['email'] . "<br>";
-            echo "joindate: " . $row['joindate'] . "<br>";
+ 
+            // Check if password's match (hashed)
+            $passMatch = password_verify($password, $row['password']);
+           
+            // They successfully logged in (correct password - w/ hashed)
+            if ($passMatch === TRUE) {
+                
+                $_SESSION['loggedin'] = true;
+                $_SESSION['username'] = $row['username'];
+                $_SESSION['joindate'] = $row['joindate'];
+                
+                echo "<h1>Account Information:</h1>";
+                echo "<p>Welcome, " . $row['username'] . "</p>";
+                echo "userID: " . $row['userID'] . "<br>";
+                echo "username: " . $row['username'] . "<br>";
+                echo "password (hashed): " . $row['password'] . "<br>";
+                echo "email: " . $row['email'] . "<br>";
+                echo "joindate: " . $row['joindate'] . "<br>";
+                
+            }
           }
-        } else {
-          echo "0 results";
+          
+          exit; // temp - for debugging
+          
+        } 
+        // They do NOT have an account
+        else {
+            
+            $conn->close();
+            
+            errorMessage("Account does not exist.");
+            
+            header("login.php");
+            exit;
+            
         }
+        
         $conn->close();
         
     }
@@ -48,8 +95,8 @@ if ($_POST) {
         
     }
     
-//    header("Location: login.php");
-//    exit;
+    header("Location: login.php");
+    exit;
 
 } 
 else {
--- user/login.php
+++ user/login.php
@@ -13,6 +13,13 @@ if (isset($_SESSION['loggedin'])) {
     exit;
 }
 
+// Vars
+$login_errorM;
+
+if (isset($_SESSION['login_errorMessage'])) {
+    $login_errorM = $_SESSION['login_errorMessage'];
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -62,11 +69,20 @@ if (isset($_SESSION['loggedin'])) {
         <main class="form-signin">
             <form action="auth.php" method="POST">
                 <img id="loginBGImg" class="mb-4" src="../images/logo_main.png" alt="" width="250" height="250">
+                <?php
+                    if (isset($login_errorM)) {
+                ?>
+                
+                <p><?= $login_errorM ?></p>
+                
+                <?php
+                    }
+                ?>
                 <h1 class="h3 mb-3 fw-normal">Enter your credentials OR create an account</h1>
                 <label for="inputUsername" class="visually-hidden">Username</label>
-                <input type="text" id="inputUsername" class="form-control" placeholder="Username" required autofocus>
+                <input type="text" id="inputUsername" class="form-control" placeholder="Username" name="uname" required autofocus>
                 <label for="inputPassword" class="visually-hidden">Password</label>
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="pwd" required>
 <!--                <div class="checkbox mb-3">
                     <label>
                         <input type="checkbox" value="remember-me"> Remember me
